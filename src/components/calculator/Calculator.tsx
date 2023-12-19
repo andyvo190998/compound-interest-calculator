5462aca32233916209ab7c4a9c6d6f94ce0e29ce
Andy Vo <andyvo@Andys-MacBook-Pro.local>
2023-12-19 17:21:45 +0100

    feat: creating formular to calculate interest rate

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import './calculator.css';
 import logo from '../../assets/logo.png';
 import { Button, TextField, Typography } from '@mui/material';
 
 const Calculator = () => {
+  const [inputValues, setInputValues] = useState({
+    initialDeposit: 0,
+    monthlyContribution: 0,
+    investmentLength: 1,
+    annualInterest: 0,
+  });
+  const handleSubmit = (e: FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const {
+      initialDeposit,
+      investmentLength,
+      annualInterest,
+      monthlyContribution,
+    } = inputValues;
+
+    const powerPart = Math.pow(
+      1 + annualInterest / 100 / 1,
+      1 * investmentLength
+    );
+    let futureValue = initialDeposit * powerPart;
+    const contribution =
+      initialDeposit + monthlyContribution * investmentLength * 12;
+    let withMonthlyContribution = 0;
+    if (monthlyContribution > 0) {
+      withMonthlyContribution =
+        (monthlyContribution * (powerPart - 1)) / (annualInterest / 100 / 1);
+    }
+    futureValue = futureValue + withMonthlyContribution * 12;
+    console.log(Math.floor(futureValue), contribution);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [name]: Number(value),
+    }));
+  };
+
   return (
     <div className="app__calculator">
       <div className="app__calculator-img">
@@ -17,20 +56,46 @@ const Calculator = () => {
           perferendis molestias. Neque reiciendis ullam laboriosam corporis
         </p>
 
-        <div className="app__calculator-form_inputs">
-          <TextField label="initial deposit" />
-          <TextField label="initial deposit" />
-          <TextField label="initial deposit" />
-          <TextField label="initial deposit" />
-          <TextField label="initial deposit" />
-          <TextField label="initial deposit" />
-        </div>
-        <Button
-          sx={{ mt: 2, width: 'fit-content', marginLeft: 'auto' }}
-          variant="contained"
-        >
-          Calculate
-        </Button>
+        <form className="app__calculator-form_inputs" onSubmit={handleSubmit}>
+          <TextField label="Compound Frequency" />
+          <TextField label="Currency" />
+          <TextField
+            value={inputValues.initialDeposit}
+            onChange={handleChange}
+            label="initial deposit"
+            name="initialDeposit"
+            type="number"
+          />
+          <TextField
+            value={inputValues.monthlyContribution}
+            onChange={handleChange}
+            label="Monthly Contribution"
+            name="monthlyContribution"
+            type="number"
+          />
+          <TextField
+            value={inputValues.investmentLength}
+            onChange={handleChange}
+            label="Investment Length (year)"
+            name="investmentLength"
+          />
+          <TextField
+            value={inputValues.annualInterest}
+            onChange={handleChange}
+            label="Annual Interest Rate (Per Year)"
+            name="annualInterest"
+            type="number"
+          />
+
+          <Button
+            sx={{ mt: 2, width: 'fit-content', marginLeft: 'auto' }}
+            variant="contained"
+            type="submit"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Calculate
+          </Button>
+        </form>
       </div>
     </div>
   );
